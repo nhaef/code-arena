c4c2306609dd676fb1eb5019ac7bf03b3a9c47ad
nah0131 <nico.haefele@privatemx.de>
2021-02-17 20:18:46 +0100

    Create LandingPage and NotFound

--- app/ui/app/src/app/app-routing.module.ts
+++ app/ui/app/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'notfound', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- app/ui/app/src/app/app.module.ts
+++ app/ui/app/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
