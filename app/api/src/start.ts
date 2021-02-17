406ca592f1662e6826082d5c1089d1db81447740
nah0131 <nico.haefele@privatemx.de>
2021-02-17 18:43:32 +0100

    Setup project structure

---
+++ app/api/src/start.ts
@@ -0,0 +1,26 @@
+import express, {Application, Router} from 'express';
+import {config} from 'dotenv';
+import path from 'path';
+
+import {apiRouter} from './routes';
+
+// Load environment
+config();
+const uiPath = path.join(path.dirname(path.dirname(__dirname)), 'ui/app/dist/app');
+
+//Define routes
+const app: Application = express();
+const router: Router = Router();
+
+// Handle API routes
+router.use('/api', apiRouter);
+// Handle UI routes
+router.use(express.static(uiPath));
+// Handle 404 routes
+router.use((req, res, next) => res.status(404).end('This page does not exist :( :( :('));
+
+app.use(router);
+app.listen(process.env.PORT);
+
+// API up and running
+console.log(`Currently listening on Port ${process.env.PORT}`);
