8fe40ef2bcb6915ba4ea81ed439e801318c89fa0
nhaef <nico.haefele@privatemx.de>
2021-02-18 18:29:49 +0100

    Landing page for code-arena (#1)
    * Create LandingPage and NotFound
    
    * Add logo
    
    * Add landing page
    
    * Refactor code

--- app/ui/app/src/app/app-routing.module.ts
+++ app/ui/app/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'signup', redirectTo: 'notfound' },
+  { path: 'notfound', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ app/ui/app/src/app/components/landing-page/landing-page.component.ts
@@ -0,0 +1,18 @@
+import { ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.scss']
+})
+export class LandingPageComponent {
+
+  constructor(private router: Router) { }
+
+  redirectToSignup() {
+    this.router.navigate(['signup']);
+  }
+
+}
