ac8e9ef135c59a66cc1c93eccd9007805d65e10b
nah0131 <nico.haefele@privatemx.de>
2021-02-18 13:20:09 +0100

    Add landing page

--- app/ui/app/src/app/app-routing.module.ts
+++ app/ui/app/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
+  { path: 'signup', redirectTo: 'notfound' },
   { path: 'notfound', component: NotFoundComponent }
 ];
 
--- app/ui/app/src/app/components/landing-page/landing-page.component.ts
+++ app/ui/app/src/app/components/landing-page/landing-page.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss']
 })
-export class LandingPageComponent implements OnInit {
+export class LandingPageComponent {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit(): void {
+  redirectToSignup() {
+    this.router.navigate(['signup']);
   }
 
 }
