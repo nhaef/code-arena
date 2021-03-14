dcc6883f7b034791d9fa2dbc0fead78eb8b5f5bc
nah0131 <nico.haefele@privatemx.de>
2021-03-14 10:31:41 +0100

    host apidoc

--- app/api/src/start.ts
+++ app/api/src/start.ts
@@ -9,12 +9,14 @@ import { apiRouter } from './routes';
 // Environment
 const port = process.env.PORT || 3000;
 const uiPath = path.join(path.dirname(path.dirname(path.dirname(path.dirname(__dirname)))), 'ui/app/dist/app');
+const apidocPath = path.join(path.dirname(path.dirname(path.dirname(__dirname))), 'apidoc');
 
 const app: Application = express();
 const router: Router = Router();
 
 // Handle API routes
 router.use('/api', apiRouter);
+router.use('/apidoc', express.static(apidocPath));
 // Handle UI routes
 router.use(express.static(uiPath));
 router.use('**', (req: Request, res: Response) => res.sendFile(path.join(uiPath, 'index.html')));
